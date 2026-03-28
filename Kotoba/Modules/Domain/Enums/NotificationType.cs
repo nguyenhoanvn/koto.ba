82b02f025443af38f53d919c002c783d6c00d8a3
nguyenhoanvn <hoanyu12345@gmail.com>
2026-03-29 04:32:10 +0700

    feat(domain): add conversation roles migration and notification types

--- Kotoba/Modules/Domain/Enums/NotificationType.cs
+++ Kotoba/Modules/Domain/Enums/NotificationType.cs
@@ -2,6 +2,7 @@ namespace Kotoba.Modules.Domain.Enums
 {
     public enum NotificationType
     {
+        NewMessage,
         NewFollower,        // User A bắt đầu follow bạn
         StoryReaction,      // User A react story của bạn
         AdminWarning,       // Admin cảnh báo bạn vì bị report
